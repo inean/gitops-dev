291ecd977a85ee6e85b94d3ca30e2c1c328c1e72
Carlos Martín <inean.es@gmail.com>
2021-07-16 17:17:24 +0200

    Fix opt parse options for brew uninstaller

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -17,20 +17,23 @@ packager_install() {
 packager_uninstall() {
   # Download and run the uninstall script.
   script_url="https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh" 
-  dry_run=""
+  opt_brew_path=""
+  opt_dry_run=""
+
   # Sanity Check. prefix is optional. If present. Ensure hombrew is installed
   if [[ ! -z $1 ]]; then
-    if [[ -x $1/brew ]]; then
-      abort '$1/brew not found'
+    if [[ ! -x $1/brew ]]; then
+      abort "$1/brew not found"
     fi
+    opt_brew_path="--path=$1"
   fi
   # Dry run script
   if [[ ! -z $2 ]]; then
-    dry_run="--dry-run"
+    opt_dry_run="--dry-run"
   fi
   curl -sLo $tmpdir/uninstall.sh $script_url || abort "failed to download script from $script_url"
   chmod +x $tmpdir/uninstall.sh
-  sudo $tmpdir/uninstall.sh -p $1 --force $dry_run
+  sudo $tmpdir/uninstall.sh --force $opt_brew_path $opt_dry_run
 
   # Clean up Homebrew directories.
   if [[ $dry_run -eq "" ]]; then
